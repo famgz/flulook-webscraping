309549b8ad3de42b336fa73ccc2db44cbc478a89
famgz <famgz@proton.me>
2024-04-23 15:52:59 -0300

    feat: pages scraping implementation

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
+from time import sleep
 import requests
 from bs4 import BeautifulSoup
-from famgz_utils import json_
-from pathlib import Path
+from famgz_utils import json_, print
+from config import products_json_path, sample_path
+from product import Product
 
-source_dir = Path(__file__).parent.resolve()
-json_path = Path(source_dir, 'data', 'products.json')
-sample_path = Path(source_dir, 'product-sample.html')
-
-if not json_path.exists():
-     json_(json_path, {})
-
-products = json_(json_path)
-
-base_url = 'https://www.flulook.com.br/loja/busca.php?loja=809533&pg=1'
+BASE_URL = 'https://www.flulook.com.br/loja/busca.php?loja=809533'
 
 
-def parse_items(items):
-    return items
+def load_products_json():
+    return json_(products_json_path) or []
+
+
+def write_products_json(products_list):
+    json_(products_json_path, products_list, ensure_ascii=False, indent=2)
 
 
 def load_sample():
@@ -25,17 +22,46 @@ def load_sample():
 
 
 def test():
-    print(load_sample())
+    item = (load_sample())
+    product = Product(item)
+
+
+def scrape():
+    pg = 1
+    products_list = load_products_json()
+    count_products_found = 0
+    count_products_new = 0
+    while True:
+        url = f'{BASE_URL}&palavra_busca=&pg={pg}'
+        print(f'[bright_blue]Page {pg}[/][bright_black]: {url}')
+        pg += 1
+        res = requests.get(url)
+        sleep(1)
+        if not res.ok:
+            print(f'[black on yellow]Unable to get page {url}\nAborting...')
+            return
+        page = BeautifulSoup(res.content, 'lxml')
+        items = page.select('li.item.flex')
+        count_products_found += len(items)
+        if not items:
+            print('No items were found. Exiting...')
+            break
+        products = [Product(item).to_dict() for item in items]
+        products_new = [x for x in products if x not in products_list]
+        count_products_new += len(products_new)
+        products_list += products_new
+        print(f'{len(items)} items -> [bright_green]{len(products)} new products')
+        write_products_json(products_list)
+        break
+    
+    print(f'\nSearch finished with [bright_blue]{count_products_found} items[/] and [purple]{count_products_new} new products')
 
 
 def main():
-    res = requests.get(base_url)
-    page = BeautifulSoup(res.content, 'lxml')
-    items = page.select('li.item.flex')
-    print(len(items))
+    scrape()
 
 
 if __name__ == '__main__':
-    test()
+    main()
 
     
