28661ca96047cc8e62775f543f4c7e47f1be7f56
famgz <famgz@proton.me>
2024-04-26 13:47:05 -0300

    feat: convert products list to products dict and added check for updated products

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ BASE_URL = 'https://www.flulook.com.br/loja/busca.php?loja=809533'
 
 
 def load_products_json():
-    return json_(products_json_path) or []
+    return json_(products_json_path) or {}
 
 
-def write_products_json(products_list):
-    json_(products_json_path, products_list, ensure_ascii=False, indent=2)
+def write_products_json(products_dict):
+    json_(products_json_path, products_dict, backup=True,
+          n_backups=3, ensure_ascii=False, indent=2)
 
 
 def load_sample():
@@ -26,14 +27,29 @@ def test():
     product = Product(item)
 
 
+def get_all_by_key(key: str):
+    products = load_products_json()
+    return list({p[key] for p in products})
+
+
+def get_products_quantity():
+    products = load_products_json()
+    return len(products)
+
+
 def scrape():
+
+    def color(items):
+        return 'bright_green' if items else 'white'
+
     pg = 1
-    products_list = load_products_json()
+    products_dict = load_products_json()
     count_products_found = 0
     count_products_new = 0
+    count_products_updated = 0
     while True:
         url = f'{BASE_URL}&palavra_busca=&pg={pg}'
-        print(f'[bright_blue]Page {pg}[/][bright_black]: {url}')
+        print(f'\n[bright_blue]Page {pg}[/][bright_black]: {url}')
         pg += 1
         res = requests.get(url)
         sleep(1)
@@ -44,19 +60,34 @@ def scrape():
         items = page.select('li.item.flex')
         count_products_found += len(items)
         if not items:
-            print('No items were found. Exiting...')
+            print('No items were found. Stopping search...')
             break
-        products = [Product(item).to_dict() for item in items]
-        products_new = [x for x in products if x not in products_list]
+        products = [Product(item) for item in items]
+        products = {p.id: p.to_dict() for p in products}
+        products_new = [
+            p_id for p_id in products
+            if p_id not in products_dict]
+        products_updated = [
+            p_id for p_id in products
+            if p_id in products_dict and products[p_id] != products_dict[p_id]]
         count_products_new += len(products_new)
-        print(f'{len(items)} items -> [bright_green]{len(products)} new products')
-        if not products_new:
-            continue
-        products_list += products_new
-        write_products_json(products_list)
-        # break
-    
-    print(f'\nSearch finished with [bright_blue]{count_products_found} items[/] and [purple]{count_products_new} new products')
+        count_products_updated += len(products_updated)
+        print(
+            f'{len(items)} items -> [{color(products_new)}]{len(products_new)} new products[/] | [{color(products_updated)}]{len(products_updated)} updated products')
+        products_dict.update(products)
+
+        if pg > 5:
+            pass
+            # break
+
+    write_products_json(products_dict)
+
+    print(
+        f'\nSearch results:'
+        f'\n[bright_blue]{count_products_found}[/] total items'
+        f'\n[bright_green]{count_products_new}[/] new products'
+        f'\n[purple]{count_products_updated}[/] updated products'
+    )
 
 
 def main():
